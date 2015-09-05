4f097150a08973a086a8e7794ce4e7681783f65f
Anton <antonpaquin@gmail.com>
2015-09-05 17:06:48 +0000

    v1.0.1

--- server/index.py
+++ server/index.py
@@ -1,5 +1,20 @@
 #! /usr/bin/python3
 import os
+from mod_python import Cookie
 
 def index(req):
-    return str(os.environ)
+    cookies = Cookie.get_cookies(req)
+    if 'User' in cookies and cookies['User'] == 'Anton':
+        return showHome(req)
+    else:
+        return showBlock(req)
+
+def showHome(req):
+    with open('Utils/HTML/main/home.html') as home_f:
+        home = home_f
+    return home
+
+def showBlock(req):
+    with open('Utils/HTML/main/block.html') as block_f:
+        block = block_f
+    return block
